1140fce33ab1b5268de956ef18d6d87be003c624
Fabian Williams <fabian@adotob.com>
2014-11-18 14:24:34 -0500

    Adding Old Project
    Now

---
+++ ReadSPWebSiteInfo/FabiansWorldTennantInfo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Online.SharePoint.TenantAdministration;
+
+namespace ReadSPWebSiteInfo
+{
+
+    /*
+     * Empty space to avoid eyes & camera when doing a session 
+     * Scroll down for real info
+     * 
+     * 
+     * 
+     */
+
+
+
+
+
+
+
+
+
+
+
+    public static class FabiansWorldTennantInfo
+    {
+        static string pw { get; set; }
+        static string PrimarySC { get; set; }
+        static string PrimaryUserName { get; set; }
+
+        public static string GetTennantUserName()
+        {
+            pw = "!#%24ZCBxv";
+            return pw;
+        }
+
+        public static string GetTennantPassword()
+        {
+            PrimaryUserName = "fg@fabiansworld.onmicrosoft.com";
+            return PrimaryUserName;
+        }
+
+        public static string GetMySite()
+        {
+            PrimarySC = "https://fabiansworld.sharepoint.com";
+            return PrimarySC;
+        }
+    }
+}
---
+++ ReadSPWebSiteInfo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace ReadSPWebSiteInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IMPORTANT: a Public Static class FabiansWorldTennantInfo is
+            //added with my Secure Tennant Info
+            //In this instance consider FabiansWorldTennantInfo.GetMySite
+            //to be https://site.sharepoint.com or the site you need it to be
+            using (var ctx = new ClientContext(FabiansWorldTennantInfo.GetMySite()))
+            {
+                //in this instance below consider mypw to be a any password but becasue it is real
+                //i am obscuring it from view as this is being viewed publcicly
+                var mypw = FabiansWorldTennantInfo.GetTennantPassword();
+                
+                var myPassword = new SecureString();
+
+                foreach (char c in mypw.ToCharArray()) myPassword.AppendChar(c);
+                ctx.Credentials = new SharePointOnlineCredentials
+                    ("fg@fabiansworld.onmicrosoft.com",myPassword);
+
+                //Lets Read some Web Info to Prove this Out
+                Web currWeb = ctx.Web;
+                //currWeb.CreateWeb("Demo1", "Demo1", "Demonstration One", "STS#1", 1033);
+                
+                ctx.Load(currWeb, w => w.Title, w => w.Description);
+                ctx.ExecuteQuery();
+                var mySiteTitle = currWeb.Title;
+
+                Console.WriteLine("My SharePoint Site Title on Fabians World is " + mySiteTitle);
+                Console.ReadLine();
+                 
+
+            }
+        }
+    }
+}
